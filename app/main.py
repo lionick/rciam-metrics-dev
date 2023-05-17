268de8e891651a94e74a62f42a97b7fb1d01fed2
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-05-17 17:14:52 +0300

    Fetch userinfo using access token

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import os
 import sys
-import time
 from pprint import pprint
+
+import requests as reqs
+
 
 from xmlrpc.client import boolean
 from fastapi import Depends, FastAPI, HTTPException, Query, Request, HTTPException, status
@@ -21,14 +23,47 @@ from app.models.country_model import *
 from app.models.idp_model import *
 from app.models.country_hashed_user_model import *
 
+from app.utils import configParser
+import urllib.parse
+from starlette.responses import HTMLResponse, RedirectResponse
+from authlib.integrations.starlette_client import OAuth, OAuthError
+
 from .routers import authenticate, communities, countries, logins, users
 
 sys.path.insert(0, os.path.realpath('__file__'))
 # Development Environment: dev
 environment = os.getenv('API_ENVIRONMENT')
 
+
+# TODO: Tenant hardcoded for now
+OIDC_config = configParser.getConfig('oidc_client_egi')
+SERVER_config = configParser.getConfig('server_config')
+oauth = OAuth()
+
+oauth.register(
+    'rciam',
+    client_id=OIDC_config['client_id'],
+    client_secret=OIDC_config['client_secret'],
+    server_metadata_url=OIDC_config['issuer'] + "/.well-known/openid-configuration",
+    client_kwargs={'scope': 'openid profile email voperson_id eduperson_entitlement'}
+)
+
 async def is_authenticated(request: Request):
-    pprint(request.headers)
+    access_token = request.headers.get('x-access-token')
+    try:
+        rciam = oauth.create_client('rciam')
+        metadata = await rciam.load_server_metadata()
+
+        headers = {'Authorization': f'Bearer {access_token}'}
+        resp = reqs.get(metadata['userinfo_endpoint'], headers=headers)
+        # pprint(resp)
+        # pprint(resp.status_code)
+        # pprint(resp.reason)
+        resp.raise_for_status()
+        data = resp.json()
+        pprint(data)
+    except Exception as er:
+        raise HTTPException(status_code=401)
 
 
 # Instantiate app according to the environment configuration
